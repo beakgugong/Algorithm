e931c74c48f259920e05a203cc0e70265598d8bb
beakgugong <86966789+beakgugong@users.noreply.github.com>
2023-08-12 16:43:32 +0900

    [level 1] Title: 모의고사, Time: 0.89 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+class Solution {
+    public ArrayList<Integer> solution(int[] answers) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int[] one = {1,2,3,4,5};
+        int num1 = 0;
+        int answer1 = 0;
+        int[] two = {2,1,2,3,2,4,2,5};
+        int num2 = 0;
+        int answer2 = 0;
+        int[] three = {3,3,1,1,2,2,4,4,5,5};
+        int num3 = 0;
+        int answer3 = 0;
+        for(int i=0; i<answers.length; i++){
+            if(num1>4){
+                num1=0;
+            }
+            if(answers[i]==one[num1]){
+                answer1++;
+            }
+            num1++;
+        }
+        for(int i=0; i<answers.length; i++){
+            if(num2>7){
+                num2=0;
+            }
+            if(answers[i]==two[num2]){
+                answer2++;
+            }
+            num2++;
+        }
+        for(int i=0; i<answers.length; i++){
+            if(num3>9){
+                num3=0;
+            }
+            if(answers[i]==three[num3]){
+                answer3++;
+            }
+            num3++;
+        }
+        if(answer1>answer2&&answer1>answer3)answer.add(1);
+        else if(answer2>answer1&&answer2>answer3)answer.add(2);
+        else if(answer3>answer1&&answer3>answer2)answer.add(3);
+        else if(answer1==answer2&&answer1>answer3){
+            answer.add(1);
+            answer.add(2);
+        }
+        else if(answer1==answer3&&answer1>answer2){
+            answer.add(1);
+            answer.add(3);
+        }
+        else if(answer2==answer3&&answer3>answer1){
+            answer.add(2);
+            answer.add(3);
+        }
+        else if(answer1==answer2&&answer1==answer3){
+            answer.add(1);
+            answer.add(2);
+            answer.add(3);
+        }
+        
+        return answer;
+    }
+}
