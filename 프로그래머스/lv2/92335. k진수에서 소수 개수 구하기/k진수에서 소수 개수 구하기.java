9a7eb60687e06d3069feae20ffe241eadc857b7a
beakgugong <86966789+beakgugong@users.noreply.github.com>
2023-08-21 16:29:38 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.14 ms, Memory: 67.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        String str = Integer.toString(n,k);
+        
+        String[] sstr = str.split("0");
+        ArrayList<Long> change = new ArrayList<>();
+        
+        for(int i=0; i<sstr.length; i++){
+            try{
+            change.add(Long.parseLong(sstr[i]));
+            }
+            catch(NumberFormatException e){
+            }
+        }
+        
+        int num = 0;
+        for(int i=0; i<change.size(); i++){
+            if(change.get(i)==1){
+                    num++;    
+                }
+            for(int j=2; j<=Math.sqrt(change.get(i)); j++){
+                
+                if(change.get(i)%j==0){
+                    num++;
+                    break;
+                }
+            }
+            if(num==0) answer++;
+            else num=0;
+        }
+        return answer;
+    }
+}
