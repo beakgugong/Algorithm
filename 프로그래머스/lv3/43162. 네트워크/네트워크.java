37c337fe2274ca8b3a9e194b7020635bb1c237ab
beakgugong <86966789+beakgugong@users.noreply.github.com>
2023-09-10 17:52:37 +0900

    [level 3] Title: 네트워크, Time: 0.44 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -0,0 +1,43 @@
+import java.util.Queue;
+import java.util.LinkedList;
+ 
+class Solution {
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+ 
+        boolean[] visited = new boolean[n];
+        Queue<Integer> queue = new LinkedList<>();
+ 
+        for (int i = 0; i < computers.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
+ 
+            queue.offer(i);
+            answer++;
+ 
+            while(!queue.isEmpty()) {
+                int cur = queue.poll();
+ 
+                if (visited[cur]) {
+                    continue;
+                }
+ 
+                visited[cur] = true;
+ 
+                int[] computer = computers[cur];
+ 
+                for (int j = 0; j < computer.length; j++) {
+                    if (visited[j] || computer[j] == 0){
+                        continue;
+                    }
+ 
+                    queue.offer(j);
+                }
+            }
+        }
+ 
+ 
+        return answer;
+    }
+}
