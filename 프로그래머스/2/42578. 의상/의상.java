a667f6cbfbe34439533857bdcfd7762f0dbc8f0e
beakgugong <86966789+beakgugong@users.noreply.github.com>
2023-10-14 15:08:12 +0900

    [level 2] Title: 의상, Time: 0.14 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<clothes.length; i++){
+            try{
+                map.put(clothes[i][1], map.get(clothes[i][1])+1);
+            }
+            catch(NullPointerException e){
+                map.put(clothes[i][1],1);
+            }
+        }
+        
+        for(String key : map.keySet()){
+            answer *= (map.get(key)+1);
+        }
+        answer-=1;
+        return answer;
+    }
+}
