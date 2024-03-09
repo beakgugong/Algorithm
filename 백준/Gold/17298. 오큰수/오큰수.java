103e3faec9ab7da79d0010fc38d28217dc53320a
beakgugong <86966789+beakgugong@users.noreply.github.com>
2024-03-09 16:27:36 +0900

    [Gold IV] Title: 오큰수, Time: 1016 ms, Memory: 146704 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -15,18 +14,25 @@ public class Main {
     Stack<Integer> stack = new Stack<>();
     int[] arr = new int[N];
     int[] answer = new int[N];
-    Arrays.fill(answer,-1);
 
-    for (int i=0; i<N; i++){
+    for (int i = 0; i < N; i++){
       arr[i] = Integer.valueOf(stringTokenizer.nextToken());
-
-      while (stack.size()>0&&arr[stack.peek()]<arr[i]){
-        answer[stack.peek()] = arr[i];
-        stack.pop();
-      }
-      stack.push(i);
     }
-    
+    for (int i=N-1; i>-1; i--){
+      if (stack.isEmpty()){
+        answer[i] = -1;
+        stack.add(arr[i]);
+        continue;
+      }
+      if (stack.peek()>arr[i]){
+        answer[i] = stack.peek();
+        stack.add(arr[i]);
+      }
+      else {
+        stack.pop();
+        i++;
+      }
+    }
     StringBuilder sb = new StringBuilder();
     for(int i=0;i<N;i++){
       sb.append(answer[i]);
