59b82cfec7aa5bf6de172aae7eca61b84b44d0ed
beakgugong <86966789+beakgugong@users.noreply.github.com>
2024-06-10 18:49:59 +0900

    [Gold V] Title: 톱니바퀴, Time: 132 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
+++ 백준/Gold/14891. 톱니바퀴/톱니바퀴.java
@@ -1,15 +1,10 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-  static int[][] map;
-  static int N;
-  static int L;
   static ArrayList<Wheel> arrayList;
-
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     arrayList = new ArrayList<>();
