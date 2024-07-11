8022f2dab13fce556613385deec7bf4b5e8625f0
beakgugong <86966789+beakgugong@users.noreply.github.com>
2024-07-11 23:23:45 +0900

    [Gold I] Title: 구간 합 구하기, Time: 668 ms, Memory: 132500 KB -BaekjoonHub

---
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  static long[] arr;
+  static long[] tree;
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+    int n = Integer.valueOf(stringTokenizer.nextToken());
+    int m = Integer.valueOf(stringTokenizer.nextToken());
+    int k = Integer.valueOf(stringTokenizer.nextToken());
+    arr = new long[n+1];
+    tree = new long[n*4];
+
+    for (int i=1; i<n+1; i++){
+      stringTokenizer = new StringTokenizer(br.readLine());
+      arr[i] = Long.valueOf(stringTokenizer.nextToken());
+    }
+
+    init(1, n, 1);
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < m + k; i++) {
+      stringTokenizer = new StringTokenizer(br.readLine());
+
+      int a = Integer.parseInt(stringTokenizer.nextToken());
+      int b = Integer.parseInt(stringTokenizer.nextToken());
+      long c = Long.parseLong(stringTokenizer.nextToken());
+
+      if (a == 1) {
+        long dif = c - arr[b];
+        arr[b] = c;
+        update(1, n, 1, b, dif);
+      } else if (a == 2) {
+        sb.append(sum(1, n, 1, b, (int) c) + "\n");
+      }
+    }
+    System.out.println(sb);
+    br.close();
+  }
+
+
+  public static long init(int start, int end, int node) {
+    if (start == end) {
+      return tree[node] = arr[start];
+    }
+
+    int mid = (start + end) / 2;
+
+
+    return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
+  }
+
+  public static long sum(int start, int end, int node, int left, int right) {
+
+    if (left > end || right < start) {
+      return 0;
+    }
+
+    if (left <= start && end <= right) {
+      return tree[node];
+    }
+
+    int mid = (start + end) / 2;
+    return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
+  }
+
+  public static void update(int start, int end, int node, int idx, long dif) {
+
+    if (idx < start || idx > end) {
+      return;
+    }
+
+
+    tree[node] += dif;
+    if (start == end) {
+      return;
+    }
+
+    int mid = (start + end) / 2;
+    update(start, mid, node * 2, idx, dif);
+    update(mid + 1, end, node * 2 + 1, idx, dif);
+  }
+}
