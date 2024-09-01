f1122a12524b4e39af05cca8273f26ad5dd2b0e5
beakgugong <86966789+beakgugong@users.noreply.github.com>
2024-09-02 02:52:34 +0900

    [Gold V] Title: 0 만들기, Time: 152 ms, Memory: 17516 KB -BaekjoonHub

--- 백준/Gold/7490. 0 만들기/0 만들기.java
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -1,50 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 
-public class Main {
-  static int num;
-  static StringBuffer stringBuffer = new StringBuffer();
+class Main {
+  static int N;
+  static int tmp;
+  static ArrayList<String> stringArrayList;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    int N = Integer.valueOf(br.readLine());
+    N = Integer.valueOf(br.readLine());
+    stringArrayList = new ArrayList<>();
 
     for (int i=0; i<N; i++){
-      num = Integer.valueOf(br.readLine());
+      tmp = Integer.valueOf(br.readLine());
 
-      dfs(1,1,"1", '1',1);
-      stringBuffer.append("\n");
+      dfs(1,1, '1',1,"1");
+      Collections.sort(stringArrayList);
+
+      for (String str : stringArrayList){
+        System.out.println(str);
+      }
+      System.out.println();
+      stringArrayList.clear();
     }
-    stringBuffer.deleteCharAt(stringBuffer.length()-1);
-    System.out.println(stringBuffer);
+
   }
-  static void dfs(int n, int sum, String str, char c, int before){
-    if (n==num){
+
+  static void dfs(int current, int sum, char c, int before, String str){
+    if (current==tmp){
       if (sum==0){
-        stringBuffer.append(str+"\n");
+        stringArrayList.add(str);
       }
       return;
     }
 
-    for (int i=0; i<3; i++){
-      if (i==0){
-        if (c=='1'){
-          dfs(n+1,sum-before+(before*10+n+1),str+ " "+(n+1),'1',(before * 10 + n + 1));
+    for (int i=0; i<3; i++) {
+      int next = current+1;
+      int blankNum = before*10;
+      
+      if (i == 0) {
+        switch (c) {
+          case '+':
+            dfs(next, sum-before+(blankNum+next), '+',blankNum+next, str+" "+next);
+            break;
+          case '-':
+            dfs(next, sum+before-(blankNum+next), '-',blankNum+next, str+" "+next);
+            break;
+          case '1':
+            dfs(next, sum-before+(blankNum+next), '1',blankNum+next, str+" "+next);
+            break;
+
         }
-        else if (c=='+') {
-          dfs(n + 1, sum - before + (before * 10 + n + 1), str + " " + (n + 1),'+',(before * 10 + n + 1));
-        }
-        else if(c=='-') {
-          dfs(n + 1, sum + before - (before * 10 + n + 1), str + " " + (n + 1),'-',(before * 10 + n + 1));
-        }
+      } else if (i==1) {
+        dfs(next,sum+next,'+',next,str+"+"+next);
       }
-      else if (i==1){
-        dfs(n+1,sum+(n+1),str+"+"+(n+1),'+',n+1);
-      }
-      else {
-        dfs(n+1, sum-(n+1),str+"-"+(n+1),'-',n+1);
-      }
+      else dfs(next,sum-next,'-',next,str+"-"+next);
     }
   }
 }
