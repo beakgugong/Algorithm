9e30b41aba6c36f1d1ef115f6c6cb0165ae09435
beakgugong <86966789+beakgugong@users.noreply.github.com>
2024-09-08 00:36:13 +0900

    [Gold IV] Title: 부분합, Time: 260 ms, Memory: 25316 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -1,42 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
     int N = Integer.valueOf(stringTokenizer.nextToken());
     int S = Integer.valueOf(stringTokenizer.nextToken());
-    int[] arr = new int[N];
-    Queue<Integer> queue = new LinkedList<>();
     stringTokenizer = new StringTokenizer(br.readLine());
+    int[] arr = new int[N];
 
     for (int i=0; i<N; i++){
       arr[i] = Integer.valueOf(stringTokenizer.nextToken());
-    }
-
-    int sum = 0;
-    int answer = Integer.MAX_VALUE;
-    for (int i=0; i<N; i++){
-      queue.add(arr[i]);
-      sum += arr[i];
-
-      if (sum>=S){
-        while (sum>=S) {
-          answer = Math.min(answer,queue.size());
-          sum -= queue.poll();
-        }
+      if (arr[i] == S) {
+        System.out.println(1);
+        return;
       }
     }
 
-    if (answer==Integer.MAX_VALUE){
-      System.out.println(0);
-      return;
+    int answer = Integer.MAX_VALUE;
+    int one = 0;
+    int two = 1;
+    int sum = arr[one]+arr[two];
+
+    while (two<N-1){
+      if (sum>=S){
+        answer = Math.min(answer,two-one);
+        sum -= arr[one++];
+      } else {
+        sum += arr[++two];
+      }
     }
-    System.out.println(answer);
+
+    if (sum>=S) {
+      while (sum>=S) {
+        sum -= arr[one++];
+        answer = Math.min(answer, two-one+1);
+      }
+    }
+
+    if (answer != Integer.MAX_VALUE) {
+      System.out.println(answer+1);
+    } else {
+      System.out.println(0);
+    }
   }
 }
