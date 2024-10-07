26ec836bfd6457e5fff08d813daf1943aafd7bca
beakgugong <86966789+beakgugong@users.noreply.github.com>
2024-10-07 14:31:12 +0900

    [Silver I] Title: 기타 레슨, Time: 296 ms, Memory: 25488 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -1,47 +1,63 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Main {
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
     int N = Integer.valueOf(st.nextToken());
     int M = Integer.valueOf(st.nextToken());
-    st = new StringTokenizer(br.readLine());
-    Integer[] arr= new Integer[N];
+    int[] minute = new int[N];
     int sum = 0;
-    int min = Integer.MAX_VALUE;
-    int low = 0;
-    for (int i=0; i<N; i++){
-      arr[i] = Integer.valueOf(st.nextToken());
-      sum += arr[i];
-      if (low<arr[i]){
-        low = arr[i];
-      }
+    int answer = Integer.MAX_VALUE;
+
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      minute[i] = Integer.valueOf(st.nextToken());
+      sum += minute[i];
     }
 
-    
-    while (low<=sum){
-      int mid = (sum+low)/2;
 
-      int minute = 0;
+    int low = 1;
+    int high = sum;
+
+    while (low <= high) {
+      int mid = (low + high) / 2;
+
       int count = 0;
-      for (int i=0; i<N; i++){
-        minute += arr[i];
-        if (minute>mid){
-          count++;
-          minute = arr[i];
+      int blueLay = 0;
+
+      for (int i=0; i<minute.length; i++) {
+        if (minute[i]>mid){
+          blueLay = M+1;
+          break;
+        }
+
+        count += minute[i];
+
+        if (count>mid){
+          blueLay++;
+          count = minute[i];
+        }else if (count==mid){
+          blueLay++;
+          count = 0;
         }
       }
-      count++;
-      if (count>M){
-        low=mid+1;
+
+      if (count<mid&&count!=0){
+        blueLay++;
       }
-      else{
-        sum=mid-1;
-        min = Math.min(min,mid);
+
+      if (blueLay>M){
+        low = mid+1;
+      } else if (blueLay<=M) {
+        high = mid-1;
+        answer = Math.min(answer, mid);
       }
     }
-    System.out.println(min);
+    System.out.println(answer);
   }
 }
