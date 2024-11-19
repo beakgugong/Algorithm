bd680c06e139d3ea119e332074d4e33b7ca4dfd7
beakgugong <86966789+beakgugong@users.noreply.github.com>
2024-11-19 11:21:41 +0900

    [Gold V] Title: 암호 만들기, Time: 184 ms, Memory: 19108 KB -BaekjoonHub

---
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static ArrayList<String> answer = new ArrayList<>();
+  static final String vowels = "aeiou";
+  static int L,C;
+  static char[] alpha;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+    L = Integer.parseInt(stringTokenizer.nextToken());
+    C = Integer.parseInt(stringTokenizer.nextToken());
+    stringTokenizer = new StringTokenizer(br.readLine());
+    alpha = new char[C];
+
+
+    for (int i = 0; i < C; i++) {
+      alpha[i] = stringTokenizer.nextToken().charAt(0);
+    }
+    Arrays.sort(alpha);
+
+    dfs(0, 0, 0, "");
+    for (String str : answer) {
+      System.out.println(str);
+    }
+  }
+
+  static void dfs(int index, int vowelCount, int consonantCount, String str) {
+    if (str.length() == L) {
+      if (vowelCount >= 1 && consonantCount >= 2) {
+        answer.add(str);
+      }
+      return;
+    }
+
+    for (int i = index; i < C; i++) {
+      char current = alpha[i];
+      if (vowels.indexOf(current) >= 0) {
+        dfs(i + 1, vowelCount + 1, consonantCount, str + current);
+      } else {
+        dfs(i + 1, vowelCount, consonantCount + 1, str + current);
+      }
+    }
+  }
+}
