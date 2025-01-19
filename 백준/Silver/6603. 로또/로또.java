da342fd9a57528362ffbfb59f56c14cf2e035793
beakgugong <86966789+beakgugong@users.noreply.github.com>
2025-01-19 17:35:15 +0900

    [Silver II] Title: 로또, Time: 144 ms, Memory: 15960 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,48 +1,47 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
-import java.util.stream.Collectors;
 
 public class Main {
+    static StringBuffer stringBuffer = new StringBuffer();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int k = Integer.parseInt(st.nextToken());
-        int[] s;
 
-        while (k != 0) {
-            s = new int[k];
-            for (int i=0; i<k; i++) {
-                s[i] = Integer.parseInt(st.nextToken());
+        while (true) {
+            int n = Integer.parseInt(st.nextToken());
+            int[] numbers = new int[n];
+
+            if (n==0)break;
+
+            for (int i = 0; i < n; i++) {
+                numbers[i] = Integer.parseInt(st.nextToken());
             }
 
-            Arrays.sort(s);
-            dfs(s,0,new ArrayList<>(),0);
-
+            printFunc(numbers);
             st = new StringTokenizer(br.readLine());
-            k = Integer.parseInt(st.nextToken());
-            System.out.println();
         }
+    }
+
+    static void printFunc(int[] numbers) {
+        Arrays.sort(numbers);
+        dfs(0, 0, "", numbers);
+        System.out.println(stringBuffer);
+        stringBuffer.setLength(0);
 
     }
-    static void dfs(int[] s, int count, ArrayList<Integer> arrayList, int index){
+    static void dfs(int index, int count, String str, int[] numbers){
         if (count==6){
-            arrayList.sort(Integer::compareTo);
-            System.out.println(arrayList.stream().map(String::valueOf).collect(Collectors.joining(" ")));
+            stringBuffer.append(str+"\n");
             return;
         }
 
-        for (int i=index; i<s.length; i++){
-            if (arrayList.indexOf(s[i])>=0){
-                continue;
-            }
-            arrayList.add(s[i]);
-            dfs(s, count+1, arrayList,i);
-            arrayList.remove(arrayList.size()-1);
+        for (int i = index; i < numbers.length; i++) {
+            dfs(i+1, count+1, str+String.valueOf(numbers[i]+" "), numbers);
         }
-
     }
 }
